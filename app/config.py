743a4e7d7b113a3e935ae37bfe8a7fbbc096eafd
leo nitro <leonardomenesesc@edu.unifor.br>
2026-07-25 16:46:44 -0300

    feat: handler_py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DYNAMODB_ENDPOINT_URL = os.getenv("DYNAMODB_ENDPOINT_URL") or None
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 POLLS_TABLE_NAME = os.getenv("POLLS_TABLE_NAME", "Polls")
